7abe473572da09ed7479269451de27d03a2f3d1b
lhopki01 <lhopki01@gmail.com>
2019-06-25 18:26:53 +0100

    Brew formula update for docker-chain-builder version v1.1.6

--- docker-chain-builder.rb
+++ docker-chain-builder.rb
@@ -2,9 +2,9 @@
 class DockerChainBuilder < Formula
   desc ""
   homepage ""
-  url "https://github.com/lhopki01/docker-chain-builder/releases/download/v1.1.3/docker-chain-builder_1.1.3_Darwin_x86_64.tar.gz"
-  version "1.1.3"
-  sha256 "47c27365fc4ad271525ab87783287423fec16f1c7a178b0b5a80265d79dbe999"
+  url "https://github.com/lhopki01/docker-chain-builder/releases/download/v1.1.6/docker-chain-builder_1.1.6_Darwin_x86_64.tar.gz"
+  version "1.1.6"
+  sha256 "096b42a9bee7680ba936a1e7fcdd08698e0683b773535fe24d144131726c4ad6"
   
   depends_on "docker"
 
